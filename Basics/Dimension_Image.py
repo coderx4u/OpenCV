9d6e0727a556ca5b8a6efde9e2e437e205264978
coderx4u <coderx4u@gmail.com>
2018-04-17 20:11:53 +0530

    Dimension of Image

---
+++ Basics/Dimension_Image.py
@@ -0,0 +1,8 @@
+import cv2
+import numpy as np
+input = cv2.imread("./Desktop/OpenCV/Basics/hand.jpg")
+# input.shape return tuples with three value. The first value represent height(x-coordinate), second value
+# represent width(y-coordinate) and third value represent returns '3L' which are the RGB values of an Image
+print input.shape
+print "Height of Image is :"int(input.shape[0])"pixels";
+print "Width of Image is :"int(input.shape[1])"pixels";
