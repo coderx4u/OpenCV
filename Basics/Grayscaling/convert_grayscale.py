180aab57ec0f833ddcff75687eaf6a33a4758751
coderx4u <coderx4u@gmail.com>
2018-04-17 22:20:46 +0530

    Colored to grey image

---
+++ Basics/Grayscaling/convert_grayscale.py
@@ -0,0 +1,9 @@
+#Grayscaling is the process of converting a full colored images into shades of grey (Black & White)
+import cv2
+input = cv2.imread("./Desktop/OpenCV/Basics/hand.jpg")
+cv2.imshow("colored image",input)
+cv2.waitKey()
+greyimage = cv2.cvtColor(input , cv2.COLOR_BGR2GRAY)
+cv2.imshow("Grey Image",greyimage)
+cv2.waitKey()
+cv2.destroyAllWindows()
